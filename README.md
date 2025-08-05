62d1273dcb64e10a7677c3914778287ca74e41a6
Tiamat <Jaycruz718@gmail.com>
2025-08-05 16:36:25 -0400

    Figured out the problem with part 1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Notes
 - Having a hard time with the first part of the lab.
 - Did the second part with no problem.
+- Figured out the problem. I looked up on Google Chrome to figure out what I was doing wrong with the first part. This made it easier to figure out the words to use.
--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // let csvStr = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
 
 //  Variables
- let cell1 = "";
- let cell2 = "";
- let cell3 = "";
- let cell4 = "";
+ // let cell1 = "";
+ // let cell2 = "";
+ // let cell3 = "";
+ // let cell4 = "";
 // let commas = 0; // Counter variable
 
 // Loop through string, for loop to have access to the index
@@ -45,26 +45,46 @@
 
 // Part 1: Refactoring Old Code
 
-// let csvStr = columnAll;
-// console.log(csv);
+let csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
+// console.log(csvData);
 
 
 // Part 2: Expanding Functionality
 
 // Declare a variable that stores the number of columns in each row of data within the CSV.
+// const rows = csvData.split('\n');
 
 // Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
-let cells = ['cell1', 'cell2', 'cell3', 'cell4'];
-
-console.log(cells);
+// const columns = rows[0].split(',').length;
 
 // Store your results in a two-dimensional array.
-// .flat();
+// let data = [];
 // Each row should be its own array, with individual entries for each column.
-
+// for (let i = 0; i < rows.length; i++) {
 // Each row should be stored in a parent array, with the heading row located at index 0.
+// let row = rows[i].split(',');
 
 // Cache this two-dimensional array in a variable for later use.
+  // data.push(row);
+// }
+// console.log(data);
+
+// Part 3: Transforming Data
+
+// Implement the following:
+// For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
+let data = [
+  ["ID", "Name", "Occupation", "Age"],
+  ["42", "Bruce", "Knight", "41"],
+  ["57", "Bob", "Fry Cook", "19"],
+  ["63", "Blaine", "Quiz Master", "58"],
+  ["98", "Bill", "Doctor’s Assistant", "26"]
+];
+
+console.log(data);
+// Convert these keys to all lowercase letters for consistency.
+// Store these objects in an array, in the order that they were originally listed.
+// Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
 
 
 
